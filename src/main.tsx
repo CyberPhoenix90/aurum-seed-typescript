d6f573ca1da2536d31bc176db55df774917b45c5
Naïm Baki <nabak19@gmail.com>
2020-08-18 10:58:31 +0200

    Added image and restyled page

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,13 @@
 import { Aurum } from "aurumjs";
 import "../scss/example.scss";
+import aurumImg from './images/aurum.png';
 
-Aurum.attach(<div>Hello Aurum</div>, document.body);
+Aurum.attach(
+    <div class="root">
+        <div class="hello">Hello Aurum</div>
+        <a href="https://aurumjs.org/" target="_blank">
+            <img src={aurumImg} class="aurum-img" />
+        </a>
+    </div>,
+    document.body
+);
